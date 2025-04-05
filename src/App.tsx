fc131c1704c4a473f7ad9afbd05300984a59bd6b
jchyeon0814 <jchyeon0814@gmail.com>
2025-04-05 10:45:04 +0900

    헤더 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 import styled from '@emotion/styled';
 import { DataView } from 'pages/DataView';
-import { TodoInput } from 'components/TodoInput';
+import { TodoInput } from 'pages/TodoInput';
 import { TodoListContextProvider } from 'contexts/TodoList';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 
 const Container = styled.div`
   height: 100vh;
@@ -20,13 +20,44 @@ const NotFound = styled.div`
   text-align: center;
 `;
 
+const Header = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+  background-color: #304ffe;
+  padding: 8px 0;
+  margin: 0;
+`;
+
+const Footer = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+  background-color: #304ffe;
+  padding: 8px 0;
+  margin: 0;
+`;
+
+const StyledLink = styled(Link)`
+  color: #ffffff;
+  font-size: 20px;
+  text-decoration: none;
+`;
+
 function App() {
   return (
     <Container>
       <TodoListContextProvider>
+        <Header>
+          <StyledLink to="/">할 일 목록 앱</StyledLink>
+        </Header>
         <Routes>
           <Route path="/" element={<DataView />} />
-          <Route path="*" element={<TodoInput />} />
+          <Route path="/add" element={<TodoInput />} />
         </Routes>
       </TodoListContextProvider>
     </Container>
--- src/components/InputContainer/index.tsx
+++ src/components/InputContainer/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { DataView } from 'pages/DataView';
-import { TodoInput } from 'components/TodoInput';
+import { TodoInput } from 'pages/TodoInput';
 import { ShowInputButton } from 'components/ShowInputButton';
 
 export const InputContainer = () => {
